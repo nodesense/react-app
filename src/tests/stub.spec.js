b7d274363ebaa09931d351433eebfde5c5ca4a27
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-01-25 17:58:02 +0530

    Create stub.spec.js

---
+++ src/tests/stub.spec.js
@@ -0,0 +1,25 @@
+describe("mock example test", () => {
+    // virtual/abstraction, given by 3rd party calc(a, b) calc can be add/sub, div...
+
+    function calculator(calc, a, b) {
+        const result = calc(a, b)
+         
+        return result;
+    }
+
+    it("mock test", () => {
+        //mock
+        const calc = jest.fn(); //returns a mock function
+        // you want to test calculator
+        
+        calculator(calc, 10, 20)
+        // goal
+        // is calc called or not
+        expect(calc).toBeCalled()
+
+        expect(calc).toBeCalledTimes(1)
+
+        expect(calc).toBeCalledWith(10, 20)
+    })
+   
+})
